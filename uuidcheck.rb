7d7706544e568b1d694043c01ab1ef822132f66c
William Urban <william.urban@navigaglobal.com>
2020-12-15 14:30:48 -0500

    version 2020-12-15T192946-4834483

--- uuidcheck.rb
+++ uuidcheck.rb
@@ -5,8 +5,8 @@
 class UUIDCheck < Formula
   desc "UUID Validator"
   homepage "https://bitbucket.org/infomaker/"
-  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/uuid-check/uuid-check-2020-12-15T191609-4834483.tar.gz"
-  sha256 "462ba1cfec5d56b4e7207ec096a3f4b2884146dadd993583ceccbd181da055af"
+  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/uuid-check/uuid-check-2020-12-15T192946-4834483.tar.gz"
+  sha256 "fef2f88b85a28f77b005e51986deb74a9399545dd7116e8c99f94e869e290995"
 
   def install
     bin.install "uuid-check"
